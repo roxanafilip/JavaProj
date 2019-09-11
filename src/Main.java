fee30ed8d8054c5e97b32f76cbc45051a560de2e
Roxy <filip.roxana95@gmail.com>
2019-09-11 21:00:19 +0300

    a

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+
+    public static void main(String[] args) {
+    System.out.println("Hello world world");
+
+    // Global information
+        String country "Italia"
+            String form "boot"
+                S
+
+
+
+
+
+
+
+
+
+    }
